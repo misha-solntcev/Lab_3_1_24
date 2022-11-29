c99601b24b827e611c734f185a9eee0d80f94146
misha-solntcev <misha.solntcev@gmail.com>
2022-11-29 08:09:15 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 24. Вычислить частичную сумму ряда S=2/(1*3)+3 / (2 * 4) + 4 / (3 * 5) +⋯
+        Вычисление прекратить, когда сумма ряда станет больше 10. */
+
+namespace Lab_3_1_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 2;
+            double sum = 0;
+            while (sum < 10)
+            {
+                sum += n / ((n-1) * (n+1));
+                Console.WriteLine(n + " " + sum);
+                n++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
